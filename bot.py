63441dae5de12d1327d610601d0f40eb49304f5f
REXOERA <rexoe573@gmail.com>
2026-09-03 06:28:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os
 import discord
-from discord import app_command
+from discord import app_commands
 from discord.ext import commands
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
